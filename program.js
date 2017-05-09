bdaa50ed0c594f26213e00f3b15bebe26fc3170b
Tobiasz12 <tobiasz63@op.pl>
2017-05-09 21:32:43 +0200

    zadanie1

--- program.js
+++ program.js
@@ -2,21 +2,21 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
 	var input = process.stdin.read();   
 	if (input !== null) {
-        var instruction = input.toString().trim();
-	    switch(instruction) {
+    	var instruction = input.toString().trim();
+		switch(instruction) {
 	    	case '/exit':
 	    		process.stdout.write('Quitting app!\n');
-	            process.exit();
+	        	process.exit();
 	       		break;
-	        case 'LANG':
+	    	case 'LANG':
 	        	console.log(process.env.LANG);
 				break;
 			case 'version':
 				console.log(process.versions.node);
 				break;
-	        default:    
-	        	process.stderr.write('Wrong instruction! ');   
+	    	default:    
+	    	process.stderr.write('Wrong instruction! ');   
 
-	    } 
-    }
+		} 
+	}
 });
