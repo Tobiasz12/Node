f3dfef05d6c2ad8c83d743c0129c111f4ea97e78
Tobiasz12 <tobiasz63@op.pl>
2017-05-10 18:00:48 +0200

    task_1.4

--- modules/timeFormat.js
+++ modules/timeFormat.js
@@ -1,7 +1,7 @@
 var os = require('os');
 var timeFormat = function (uptime) {
 	var uptime = os.uptime();
-    hours = Math.floor(uptime / 3600);
+   	hours = Math.floor(uptime / 3600);
 	uptime %= 3600;
 	minutes = Math.floor(uptime / 60);
 	seconds = uptime % 60;
